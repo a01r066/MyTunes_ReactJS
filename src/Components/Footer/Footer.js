b25cbf36f66598e2d5a96bb8c08272ae3e9039bb
Thanh Minh <vmthanh24@gmail.com>
2021-02-06 11:12:49 +0700

    initial commit

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <div> 
+         <footer className="sticky-footer bg-white">
+        <div className="container my-auto">
+          <div className="copyright text-center my-auto">
+            <span>Copyright © Your Website 2020</span>
+          </div>
+        </div>
+      </footer>
+      </div>
+    );
+  }
+}
